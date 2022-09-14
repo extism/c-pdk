0e49da7649a40db01d4b2f0dd005955e92c4aec2
Steve Manuel <steve@dylib.so>
2022-09-14 12:33:06 -0600

    ci: test example c plugin in workflow

--- example/count_vowels.c
+++ example/count_vowels.c
@@ -1,20 +1,22 @@
 #include "../extism-pdk.h"
 
-#include <stdio.h>
-
-int32_t count_vowels() {
+int32_t count_vowels()
+{
   uint64_t length = extism_input_length();
 
-  if (length == 0) {
+  if (length == 0)
+  {
     return 0;
   }
 
   int64_t count = 0;
   char ch = 0;
-  for (int64_t i = 0; i < length; i++) {
+  for (int64_t i = 0; i < length; i++)
+  {
     ch = extism_input_load_u8(i);
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
       count += 1;
     }
   }
