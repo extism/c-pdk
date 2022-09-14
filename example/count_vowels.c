6f283964f35c53fbd9cdf678ea26de2ca9cb95a1
zach <zachshipko@gmail.com>
2022-09-14 11:46:25 -0700

    feat: new input interface, add logging functions (#1)
    * chore: update for pdk changes
    
    * feat: add logging
    
    * feat: add example code from extism repo
    
    * ci: test example c plugin in workflow
    
    * ci: use emscripten installer from action
    
    * ci: add c header back to example code
    
    * ci: remove needless checkout from workflow
    
    Co-authored-by: Steve Manuel <steve@dylib.so>

---
+++ example/count_vowels.c
@@ -0,0 +1,34 @@
+#include "../extism-pdk.h"
+
+#include <stdio.h>
+
+int32_t count_vowels()
+{
+  uint64_t length = extism_input_length();
+
+  if (length == 0)
+  {
+    return 0;
+  }
+
+  int64_t count = 0;
+  char ch = 0;
+  for (int64_t i = 0; i < length; i++)
+  {
+    ch = extism_input_load_u8(i);
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+    {
+      count += 1;
+    }
+  }
+
+  char out[128];
+  int n = snprintf(out, 128, "{\"count\": %lld}", count);
+
+  uint64_t offs_ = extism_alloc(n);
+  extism_store(offs_, (const uint8_t *)out, n);
+  extism_output_set(offs_, n);
+
+  return 0;
+}
